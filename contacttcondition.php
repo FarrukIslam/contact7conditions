6f21cd4183d6c73202bd8c4f27318b028ee7b56c
FarrukIslam <farrukislam1@gmail.com>
2017-08-23 03:45:46 +0600

    uplad some files

---
+++ contacttcondition.php
@@ -0,0 +1,107 @@
+<?php <h2>What can we help you with?</h2>
+[select* help_form include_blank class:form-control "Purchase a Property" "Community Management Requests" "Lease a Retail Space" "Lease a Commercial Space" "Register for Email Alerts" "General Enquiries" "Investor Enquiries" "Media Enquiries"]
+
+[group group-1]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-2]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-3]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-4]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-5]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-6]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-7]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
+[group group-8]
+<div class="form_wrapper">
+<hr>
+<h4 class="title">Contact Details</h4>
+<div class="form-group">[text your-name class:form-control placeholder "Full Name"]</div>
+<div class="form-group">[email your-email class:form-control placeholder "Email"]</div>
+<div class="form-group">[tel your-phone class:form-control "Mobile Number"]</div>
+<hr>
+<h4 class="title">Message</h4>
+<div class="form-group">[textarea your-message class:form-control placeholder "Type your enquiry here."]</div>
+<div class="form-group">[submit id:submit class:form-control "Submit"]</div>
+</div>
+[/group]
